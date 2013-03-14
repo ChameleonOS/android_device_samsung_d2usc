50c4679dff6bf630df4b675ffa97f87b601802f8
Wizard0f0s <jamie.gilmer@gmail.com>
2013-03-14 12:06:34 -0400

    Port from CM to COS

--- cos.mk
+++ cos.mk
@@ -1,16 +1,16 @@
 $(call inherit-product, device/samsung/d2usc/full_d2usc.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/cdma.mk)
+# Inherit some common COS stuff.
+$(call inherit-product, vendor/cos/config/cdma.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/cos/config/nfc_enhanced.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common COS stuff.
+$(call inherit-product, vendor/cos/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=d2usc TARGET_DEVICE=d2usc BUILD_FINGERPRINT="d2usc-user 4.1.1 JRO03L R530UVXALK5 release-keys" PRIVATE_BUILD_DESC="samsung/d2usc/d2usc:4.1.1/JRO03L/R530UVXALK5:user/release-keys"
 
-PRODUCT_NAME := cm_d2usc
+PRODUCT_NAME := cos_d2usc
 PRODUCT_DEVICE := d2usc
 
